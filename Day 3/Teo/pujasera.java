cc99c0f512f5faaf87138e0d064c0c95d1ca6e92
AlqanNazra <151588236+AlqanNazra@users.noreply.github.com>
2024-09-02 22:18:45 +0700

    memasukkan prgram baba 3

--- Day 3/Teo/pujasera.java
+++ Day 3/Teo/pujasera.java
@@ -1,53 +1,49 @@
 import java.util.Scanner;
 
 class Menu {
-    private String barang;  // String harus dengan huruf kapital
+    private String barang;  
     private int jumlah;
     private int harga;
     
-    // Setter untuk barang
+    
     public void setBarang(String isibarang) {
         barang = isibarang;
     }
 
-    // Setter untuk harga
     public void setHarga(int isiharga) {
         harga = isiharga;
     }
 
-    // Getter untuk barang
-    public String getBarang() {
+        public String getBarang() {
         return barang;
     }
-
-    // Getter untuk jumlah
+    
     public int getJumlah() {
         return jumlah;
     }
-
-    // Setter untuk jumlah
+    
     public void setJumlah(int setjumlah) {
         jumlah = setjumlah;
     }
-
-    // Getter untuk harga
+    
     public int getHarga() {
         return harga;
     }
-
-    // Metode untuk menghitung total harga
+   
     public int hitungTotal(int jumlah, int harga) {
         return harga * jumlah;
     }
 
-    // Metode untuk mencetak menu
     public void cetakMenu() {
-        System.out.println("Menu Pujasera");
-        System.out.println(getBarang() + " - " + getJumlah() + " porsi - Harga: " + getHarga() + " Total = " + hitungTotal(getJumlah(), getHarga()));
-    }
+        System.out.println("Sturk pembelian");
+        System.out.println(getBarang() + " - " + getJumlah() + " porsi ");
+        System.out.println("Harga per makanan: " + getHarga());
+        System.out.println("Total = " + hitungTotal(getJumlah(), getHarga()));
+}
 }
 
-public class pujasera {
+public class pujasera 
+{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Menu menu = new Menu();
@@ -66,8 +62,7 @@ public class pujasera {
         System.out.print("Jumlah makanan: ");
         int jumlah_order = scanner.nextInt(); 
         menu.setJumlah(jumlah_order);
-
-
+ 
         switch (pilihan) {
             case 1:
                 menu.setBarang("Nasi Goreng");
